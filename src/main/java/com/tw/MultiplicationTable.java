fdb69b432a14825060293c2c30fadd1fec467da1
ALFRED CANCEJO (GEDI-ISD-OOCLL/MNL) <CANCEAL@OOCL.COM>
2020-10-13 18:48:43 +0800

    [Implement Multiplication Table] [Alfred Cancejo] Feature: Add functions on the MultiplicationTable.java

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,35 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
-        return null;
+        String multiplicationTable = "";
+        boolean isInputInOrder = isInputInOrder(startNumber, endNumber);
+        boolean isInputInRange = isInputInRange(startNumber, endNumber);
+
+        for (int i = startNumber; i <= endNumber; i++) {
+            for (int j = startNumber; j <= i; j++) {
+                multiplicationTable += j + "*" + i + "=" + (j * i);
+
+                if (i != j) {
+                    multiplicationTable += "  ";
+                }
+            }
+            multiplicationTable += "\r\n";
+        }
+
+        return isInputInOrder && isInputInRange ? multiplicationTable.trim(): null;
     }
+
+    public boolean isInputInRange(int startNumber, int endNumber) {
+        return (startNumber > 0 && startNumber <= 1000) && (endNumber > 0 && endNumber <= 1000);
+    }
+
+    public boolean isInputInOrder(int startNumber, int endNumber) {
+        return startNumber <= endNumber;
+    }
+
+//    public static void main (String[]args) {
+//        MultiplicationTable multiplicationTable = new MultiplicationTable();
+//        System.out.println(multiplicationTable.create(2, 4));
+//    }
+
 }
